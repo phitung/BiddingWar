bc3618731e68eebd0cef336e383996f405cb538a
ak <ak@gatgu2.domain_not_set.invalid>
2013-08-21 23:40:15 -0700

    cleaned up code

--- bidding/server.js
+++ bidding/server.js
@@ -1,14 +1,7 @@
 var io = require('socket.io').listen(3000);
 var easyamqp = require('easy-amqp');
 var connection = easyamqp.createConnection({ host: 'localhost', port: 5672, login: 'guest', password: 'guest', vhost: '/' }, { defaultExchangeName: "amq.topic" });
-
-io.sockets.on('connection', function (socket) {
-
-});
+	
 connection.queue('helloqueue').bind('logs-exchange', '#').subscribe(function(message, headers, deliveryInfo, rawMessage, queue) {
-  var encoded_payload = unescape(message.data);
-  var payload = JSON.parse(encoded_payload);
-  console.log('Recieved a message:');
-  console.log(payload);
-  io.sockets.emit('update-bids', payload);
+	io.sockets.emit('update-bids', JSON.parse(unescape(message.data)));
 });
